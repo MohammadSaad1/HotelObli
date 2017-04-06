9b394f4c2599a5e51483b19fc154f2a2258ea1d5
Mohammad Hussin Saad <moha2896@edu.easj.dk>
2017-04-06 14:21:45 +0200

    *Nye branch

--- HotelObli/CRUD.cs
+++ HotelObli/CRUD.cs
@@ -5,6 +5,10 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.UI.Notifications;
+using Newtonsoft.Json;
+using HotelObli.Models;
+
+
 
 namespace HotelObli
 {
@@ -18,7 +22,21 @@ namespace HotelObli
             {
                 client.BaseAddress = new Uri(serverUrl);
                 client.DefaultRequestHeaders.Clear();
+
+                var response = client.PostAsJsonAsync<Guest>("API/Guests", null ).Result;
+                
+                if (response.IsSuccessStatusCode)
+                {
+                    string successresponse = $"Hotel indsat ({response.Content.ReadAsStringAsync()})";
+                }
+                else
+                {
+                    string failresponse = $"Hotel ikke indsat({response.StatusCode})";
+                }
+            
             }
+           
+           
 
         } }
 }
--- HotelObli/MainPage.xaml.cs
+++ HotelObli/MainPage.xaml.cs
@@ -24,7 +24,7 @@ namespace HotelObli
     {
         public MainPage()
         {
-            this.InitializeComponent();
+            
         }
     }
 }
